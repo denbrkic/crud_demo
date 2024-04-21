f632d534d9a6aaed8124ab488d66d40becf6aea9
Denis Brkic <denis.brkic.main@gmail.com>
2024-04-21 15:19:58 +0200

    add key to box component on the homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -30,7 +30,7 @@ function HomePage() {
     return (
         <>            
             <Typography variant="h2" gutterBottom>List Of Items:</Typography>
-            { items.map((item) => <Box component="section" sx={{ p: 2, border: '1px dashed grey' }} className="item"><Link to={`/details/${item.id}`} key={item.id}><Typography variant="h3" gutterBottom>{item.title}</Typography><Typography variant="body1" gutterBottom>{item.body}</Typography></Link></Box>) }
+            { items.map((item) => <Box component="section" sx={{ p: 2, border: '1px dashed grey' }} className="item" key={item.id}><Link to={`/details/${item.id}`} key={item.id}><Typography variant="h3" gutterBottom>{item.title}</Typography><Typography variant="body1" gutterBottom>{item.body}</Typography></Link></Box>) }
             <Typography variant="body2" gutterBottom className={error.type}>{ error.message }</Typography>              
         </>
     )
